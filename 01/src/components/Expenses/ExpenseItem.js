df6ecb407c40ffef60e564563262ccea11092931
Murilo Conde <murilo@beecloud.com.br>
2022-04-18 22:30:46 -0300

    updated card import name

--- 01/src/components/Expenses/ExpenseItem.js
+++ 01/src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,18 @@
 import "./ExpenseItem.css";
 import ExpenseItemDate from "./ExpenseItemDate";
-import ExpenseCard from "../UI/Card";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   const {date, name, value} = props;
 
   return (
-    <ExpenseCard className="expense-item">
+    <Card className="expense-item">
       <ExpenseItemDate date={date}/>
       <div className="expense-item__description">
         <h2>{name}</h2>
         <div className="expense-item__price">R$ {value}</div>
       </div>
-    </ExpenseCard>
+    </Card>
   );
 }
 
--- 01/src/components/Expenses/ExpenseItemList.js
+++ 01/src/components/Expenses/ExpenseItemList.js
@@ -1,17 +1,17 @@
 import "./ExpenseItemList.css";
 
-import ExpenseCard from "../UI/Card";
+import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 
 const ExpenseItemList = (props) => {
   const {expenses} = props;
 
   return (
-    <ExpenseCard className="expense-list">
+    <Card className="expense-list">
       {expenses.map((expense) => {
         return <ExpenseItem {...expense} />;
       })}
-    </ExpenseCard>
+    </Card>
   );
 }
 
