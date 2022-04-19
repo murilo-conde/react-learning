bef99da8f5911ed9e9f2ae5e65b9a9e225299ce3
Murilo Conde <murilo@beecloud.com.br>
2022-04-18 21:43:05 -0300

    added styles to expense item date component

--- 01/src/components/ExpenseItemDate.js
+++ 01/src/components/ExpenseItemDate.js
@@ -1,14 +1,16 @@
+import './ExpenseItemDate.css'
+
 function ExpenseItemDate(props) {
   const { date } = props;
   const day = date.toLocaleString("pt-BR", { day: "2-digit" });
-  const month = date.toLocaleString("pt-BR", { month: "short" });
+  const month = date.toLocaleString("pt-BR", { month: "long" });
   const year = date.getFullYear();
 
   return (
-    <div>
-      <div>{month}</div>
-      <div>{day}</div>
-      <div>{year}</div>
+    <div className="expense-date">
+      <div className="expense-date__day">{day}</div>
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
     </div>
   );
 }
