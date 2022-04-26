79409931c2705af178fbe531bd52e9ac56728798
Murilo Conde <murilo@beecloud.com.br>
2022-04-25 21:14:58 -0300

    added dynamic button styling

--- 02/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 02/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,40 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react'
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from '../../UI/Button/Button'
+import './CourseInput.css'
 
 const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+  const [enteredValue, setEnteredValue] = useState('')
+  const [isValid, setIsValid] = useState(false)
 
   const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    const value = event.target?.value?.trim()
+
+    if (value && value.length > 0) {
+      setEnteredValue(event.target.value)
+      setIsValid(true)
+    } else {
+      setIsValid(false)
+    }
+  }
 
   const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    event.preventDefault()
+    if (enteredValue.trim().length === 0) {
+      return
+    }
+    props.onAddGoal(enteredValue)
+  }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input type="text" onChange={goalInputChangeHandler}/>
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type="submit" disabled={!isValid}>Add Goal</Button>
     </form>
-  );
-};
+  )
+}
 
-export default CourseInput;
+export default CourseInput
