54da81815a55a5c8a33164afcfa8b09524f5a60f
Murilo Conde <murilo@beecloud.com.br>
2022-04-25 21:20:59 -0300

    updated button component to use styled components

--- 02/src/components/UI/Button/Button.js
+++ 02/src/components/UI/Button/Button.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import styled from "styled-components"
 
 import './Button.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} disabled={props.disabled} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+const Button = styled.button`
+
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+
+  &:disabled {
+    background: #836983;
+    border-color: #836983;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+    cursor: default;
+  }
+`
 
 export default Button;
