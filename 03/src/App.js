1f2b1d297ffefd11df18f070d037e58e4610f23f
Murilo Conde <murilo@beecloud.com.br>
2022-04-27 09:21:23 -0300

    created user list and error modal components

--- 03/src/App.js
+++ 03/src/App.js
@@ -1,9 +1,21 @@
 import AddUser from "./components/Users/AddUser"
+import UserList from "./components/Users/UserList"
+import {useState} from "react"
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  const onUserAdd = (data) => {
+    setUsers((prev) => [
+      ...prev,
+      data
+    ])
+  }
+
   return (
     <div>
-      <AddUser/>
+      <AddUser onUserAdd={onUserAdd}/>
+      <UserList users={users}/>
     </div>
   )
 }
--- 03/src/components/Users/AddUser.js
+++ 03/src/components/Users/AddUser.js
@@ -1,44 +1,68 @@
 import {useForm} from "react-hook-form"
-import {useEffect} from "react"
+import {useCallback, useEffect, useState} from "react"
 
 import Card from "../UI/Card"
 import Button from "../UI/Button"
 
 import styles from './AddUser.module.css'
+import ErrorModal from "../UI/ErrorModal"
 
 const defaultValues = {
   username: "",
   age: null
 }
 
-const AddUser = () => {
+const AddUser = ({onUserAdd}) => {
   const {register, handleSubmit, reset, setFocus, formState: {isSubmitSuccessful}} = useForm({defaultValues})
+  const [showErrorModal, setShowErrorModal] = useState(false)
+  const [errorMessages, setErrorMessages] = useState([])
 
   const onSubmit = (data) => {
     data.age = +data.age
-    console.log(data)
+    onUserAdd(data)
 
     reset(defaultValues)
   }
+
+  const onError = (errors) => {
+    let msgs = []
+
+    for (let err in errors) {
+      msgs.push({
+        key: err,
+        message: `[${err}] ` + (errors[err].type === 'required' ? 'Required field missing!' : errors[err].message)
+      })
+    }
+
+    setErrorMessages(msgs)
+    setShowErrorModal(true)
+  }
+
+  const onClose = useCallback(() => {
+    setShowErrorModal(false)
+  }, [setShowErrorModal])
 
   useEffect(() => {
     setFocus("username")
   }, [setFocus, isSubmitSuccessful])
 
   return (
-    <Card className={styles.card}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" {...register("username", {
-          required: true, validate: value => value?.trim().length !== 0
-        })}/>
+    <>
+      <ErrorModal title="Error submitting form!" messages={errorMessages} visible={showErrorModal} onClose={onClose}/>
+      <Card className={styles.card}>
+        <form onSubmit={handleSubmit(onSubmit, onError)}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" {...register("username", {
+            required: true, validate: value => value?.trim().length !== 0 || 'Field cannot be blank!'
+          })}/>
 
-        <label htmlFor="age">Age</label>
-        <input id="age" type="number" step="1" {...register("age", {required: true, valueAsNumber: true, min: 1})}/>
+          <label htmlFor="age">Age</label>
+          <input id="age" type="number" step="1" {...register("age", {required: true, valueAsNumber: true, min: 1})}/>
 
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </>
   )
 }
 
